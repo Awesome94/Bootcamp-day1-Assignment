2cdaa1afaed0de302e307d7ff37ff4e074c1c8e8
Awesome94 <suubidavid@gmail.com>
2016-10-10 16:36:25 +0300

    Add files via upload

---
+++ fibbonacci gen.py
@@ -0,0 +1,11 @@
+#fibonnaci generator function 
+def fib():
+    a, b = 0, 1
+    while True:            # First iteration:
+        yield a            # yield 0 to start with and then
+        a, b = b, a + b
+#implementation of the function above
+for index, fibonacci_number in enumerate(fib()):
+     print('{i:3}: {f:3}'.format(i=index, f=fibonacci_number))
+     if index == 10:
+         break
